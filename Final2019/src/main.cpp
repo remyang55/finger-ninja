d2fdca1193e2b8ad4b9d6c21d25aa0b9a73d8471
RemLaptop <remyang2@illinois.edu>
2019-11-22 13:36:38 -0600

    Setup Azure Speech SDK

--- Final2019/src/main.cpp
+++ Final2019/src/main.cpp
@@ -1,13 +1,38 @@
-#include "ofMain.h"
-#include "ofApp.h"
+#include <iostream>
+#include <speechapi_cxx.h>
 
-//========================================================================
-int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+using namespace Microsoft::CognitiveServices::Speech;
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
+//docs.microsoft.com/en-us/azure/
+//cognitive-services/speech-service/quickstarts/speech-to-text-from-microphone
+void RecognizeSpeech() {
+    auto config = SpeechConfig::FromSubscription("175cc10306674657b22547951918f2e5", "centralus");
+    auto recognizer = SpeechRecognizer::FromConfig(config);
+    
+	std::cout << "Say something...\n";
+    auto result = recognizer->RecognizeOnceAsync().get();
 
+    if (result->Reason == ResultReason::RecognizedSpeech) {
+        std::cout << "You said: " << result->Text << std::endl;
+    } else if (result->Reason == ResultReason::NoMatch) {
+        std::cout << "NOMATCH: Speech could not be recognized." << std::endl;
+    } else if (result->Reason == ResultReason::Canceled) {
+        auto cancellation = CancellationDetails::FromResult(result);
+        std::cout << "CANCELED: Reason=" << (int) cancellation->Reason << std::endl;
+
+        if (cancellation->Reason == CancellationReason::Error) {
+            std::cout << "CANCELED: ErrorCode= " << (int)cancellation->ErrorCode
+                 << std::endl;
+            std::cout << "CANCELED: ErrorDetails=" << cancellation->ErrorDetails
+                 << std::endl;
+            std::cout << "CANCELED: Did you update the subscription info?"
+                 << std::endl;
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    RecognizeSpeech();
+    return 0;
 }
